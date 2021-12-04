437378a47005e39be96a5c1ec47fbe07bdda825a
hemanthsuryya <hemanthsurya10500@gmail.com>
2021-12-05 01:42:01 +0530

    saving user profiles in moralis db along with avatar image

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ Moralis.start({ serverUrl, appId });
 /* TODO: Add Moralis Authentication code */
 init = async ()  => {
     hideElement(userInfo);
-    window.web3 = await Moralis.Web3.enable();
+    window.web3 = await Moralis.enableWeb3();
     initUser();
 }
 
@@ -36,12 +36,46 @@ logout = async () => {
 }
 
 openUserInfo = async () => {
-    user = await Moralis.User.current();
+    user = await Moralis.User.current();    
     if(user){
+        const email = user.get("email");
+        if(email){
+            userEmail.value = email;
+        }
+        else{
+            userEmail.value = "";
+        }
+        userUsername.value = user.get('username');
+        const userAvatar = user.get('avatar');
+        if(userAvatar)
+        {
+            userAvatarImg.src = userAvatar.url();
+            showElement(userAvatarImg);
+        }
+        else
+        {
+            hideElement(userAvatarImg);
+        }
         showElement(userInfo);
     }else{
         login();
     }
+}
+
+saveUserInfo = async () => {
+    user.set("email", userEmail.value);
+    user.set("username", userUsername.value);
+    // const fileUploadControl = $("#profilePhotoFileUpload")[0];
+
+    if (userAvatarFile.files.length > 0) {
+        const file = userAvatarFile.files[0];
+        const name = "avatar.jpg";
+        const avatar = new Moralis.File(name, file);
+        user.set('avatar', avatar);
+    }
+    await user.save();
+    alert('User info saved successfully!!');
+    openUserInfo();
 }
 
 hideElement = (element) => element.style.display  = "none";
@@ -53,6 +87,12 @@ const userProfileButton = document.getElementById("btn-userInfo");
 userProfileButton.onclick = openUserInfo;
 
 const userInfo = document.getElementById("userInfo");
+const userUsername = document.getElementById("txt-username");
+const userEmail = document.getElementById("txt-email");
+const userAvatarImg = document.getElementById("imgAvatar");
+const userAvatarFile = document.getElementById("fileAvatar");
+
+const userSaveInfoButton = document.getElementById("btn-saveUserInfo"); userSaveInfoButton.onclick = saveUserInfo;
 document.getElementById("btn-closeUserInfo").onclick = () => hideElement(userInfo);
 document.getElementById("btn-logout").onclick = logout;
 init();
